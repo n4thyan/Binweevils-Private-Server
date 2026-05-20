40b15db21811bcb2d8b6c578c41b78f407e17410
Nathan May <uwunathan@gmail.com>
2026-05-20 08:16:10 +0100

    Add banked XP multi-level progression

--- game-full/nest/level-up.php
+++ game-full/nest/level-up.php
@@ -4,15 +4,23 @@ include('../essential/backbone.php');
 
 if(isset($_COOKIE['weevil_name'])) {
     $weevil = $_COOKIE['weevil_name'];
-    $weevilData = getAllWeevilStatsByName($weevil);
-    if($weevilData['xp'] >= $weevilData['xp2']){
+    $previousData = getAllWeevilStatsByName($weevil);
+
+    if($previousData['xp'] >= $previousData['xp2']){
+        $previousLevel = intval($previousData['level']);
         $levelled = levelWeevil($weevil);
+
         if($levelled == true){
             $weevilData = getAllWeevilStatsByName($weevil);
-            $alrtMsg = '<a href="event:weevil|'.strval($weevilData['id']).'\">'.$weevil.'</a> has reached level '.strval($weevilData['level']).'!';
+            $newLevel = intval($weevilData['level']);
+
+            for($level = $previousLevel + 1; $level <= $newLevel; $level++) {
+                rewardUserTrophy($weevil, $weevilData['id'], $level);
+            }
+
+            $alrtMsg = '<a href=\"event:weevil|'.strval($weevilData['id']).'\">'.$weevil.'</a> has reached level '.strval($weevilData['level']).'!';
             $icon = 'cdn.binw.net/users/o_levelTrophy'.strval($weevilData['level']).'_thumb.swf';
 
-            rewardUserTrophy($weevil, $weevilData['id'], $weevilData['level']);
             sendAlert($weevil, $alrtMsg, $icon, time());
             $st = strval(rand(1000000, 9999999));
             echo "level=".$weevilData['level']."&mulch=".$weevilData['mulch']."&xp=".$weevilData['xp']."&xp1=".$weevilData['xp1']."&xp2=".$weevilData['xp2']."&st=" . $st . "&hash=" . md5('P07aJK8soogA815CxjkTcA==' . $weevilData['level'] . $weevilData['mulch'] . $weevilData['xp'] . $weevilData['xp1'] . $weevilData['xp2'] . $st) . "&x=y";
